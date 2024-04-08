87d28c28244a033fe88a84cc5e6384f8bcd8d8db
Scarry <scaryberryfoundyourspaghetti@proton.me>
2024-04-08 08:36:15 +0200

    just some trials

--- app/page.js
+++ app/page.js
@@ -2,9 +2,16 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Page() {
-  return (
+  return (<html>
+    <div className={styles.header}>
+
+    </div>
     <div className={styles.main}>
       <p>HELLO</p>
     </div>
+    <div className={styles.nav}>
+
+    </div>
+    </html>
   );
 }
